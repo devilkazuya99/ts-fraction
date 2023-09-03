3f6d4ab76c2a36b4410fb49ecbf8adc102a25627
devilkazuya99 <devilkazuya99@gmail.com>
2023-09-03 14:47:37 +0800

    can convert 1/3 to 0.(3) but not the other way around.

--- src/fraction.helper.spec.ts
+++ src/fraction.helper.spec.ts
@@ -9,9 +9,9 @@ describe('fraction.helper', () => {
 
     [
         [{ a: '' }, false],
-        [{ d: '', n: '', s: '' }, false],
-        [{ s: 3, n: 3 }, false],
-        [{ n: 3, d: 5 }, true]
+        [{ denominator: '', numerator: '', sign: '' }, false],
+        [{ sign: 3, numerator: 3 }, false],
+        [{ numerator: 3, denominator: 5 }, true]
     ].forEach(([x, e]) => {
         it(`isParseData: ${JSON.stringify(x)}`, () => {
             expect(isParseData(x)).toBe(e as boolean);
@@ -25,21 +25,13 @@ describe('fraction.helper', () => {
     it('convertFloatToFraction', () => {
         const frc = convertFloatToFraction(3.142);
         console.log(frc);
-        expect(frc).toEqual(jasmine.objectContaining({ s: 1, n: 1571, d: 500 }));
+        expect(frc).toEqual(jasmine.objectContaining({ sign: 1, numerator: 1571, denominator: 500 }));
     });
 
     it('convertFloatToFraction', () => {
         const frc = convertFloatToFraction(-17.345);
         console.log(frc);
-        expect(frc).toEqual(jasmine.objectContaining({ s: -1, n: 3469, d: 200 }));
-    });
-
-    it('parse', () => {
-        const frc = new Fraction("-17.(345)");
-        console.log("-17.(345) --> ", frc);
-        const frc2 = new Fraction("-17.(345)");
-        console.log('frc2 = ', frc2);
-        console.log(frc2.toString(4));
+        expect(frc).toEqual(jasmine.objectContaining({ sign: -1, numerator: 3469, denominator: 200 }));
     });
 
 });
